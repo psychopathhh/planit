59e111835cb79f4e0d6bfae82caba2a0f47b58de
Daria <badrenkovadasha@yandex.ru>
2023-02-04 17:06:29 +0300

    sidebar is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, ThemeSettings, Sidebar, Footer } from './components'
+import { Calendar, Kanban, Editor } from './pages'
+import './App.css'
+
+const App = () => {
+    const activeMenu = true
+    return (
+        <div>
+            <BrowserRouter>
+                <div className='flex relative dark:bg-main-dark-bg'>
+                    <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+                        <TooltipComponent content='Settings' position='Top'>
+                            <button type='button' className='text-3xl hover:drop-shadow-xl p-3 
+                            hover:bg-light-gray text-white '
+                                style={{ background: 'blue', borderRadius: '50%' }}
+                            >
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
+                    {
+                        activeMenu ? (
+                            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                                <Sidebar />
+                            </div>
+                        ) : (
+                            <div className='w-0 dark:bg-secondary-dark-bg'>
+                                <Sidebar />
+                            </div>
+                        )
+                    }
+                    <div className={
+                        `dark:bg-main-bg w-full bg-main-bg min-h-screen ${activeMenu ? ' md:ml-72' : 'flex-2'}`
+                    }>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            <Navbar />
+                        </div>
+                    </div>
+                    <div>
+                        <Routes>
+                            <Route path='/' element={<Kanban />} />
+                            <Route path='/kanban' element={<Kanban />} />
+                            <Route path='/editor' element={<Editor />} />
+                            <Route path='/calendar' element={<Calendar />} />
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Kanban } from './Kanban';
+export { default as Editor } from './Editor';
+export { default as Calendar } from './Calendar';
